067c8e1e5aad11adee0a7006203d5cbe5a26012b
Tafhimbn <tafhim.aiub@gmail.com>
2025-10-02 01:33:13 +0600

    .

--- 0_intro/obj/Debug/net8.0-windows/0_intro.AssemblyInfo.vb
+++ 0_intro/obj/Debug/net8.0-windows/0_intro.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("0_intro"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290dd8bae421527a2ba65e700bbf6ffdf7210065"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4472ca8a63bbdee7f3606c0e9b747d6afe7ded6e"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("0_intro"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("0_intro"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
